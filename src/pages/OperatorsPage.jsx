71d76b346bdd2bd020c7131c0480301ff757f805
hoshikira18 <khuyen.dev183@gmail.com>
2025-03-25 10:46:59 +0700

    refactor: link tag

--- src/pages/OperatorsPage.jsx
+++ src/pages/OperatorsPage.jsx
@@ -292,13 +292,12 @@ const OperatorsPage = () => {
                                 </td>
                                 <td className="p-2 text-center">
                                     <span
-                                        className={`px-2 py-1 rounded-full ${
-                                            operator.status === "active"
+                                        className={`px-2 py-1 rounded-full ${operator.status === "active"
                                                 ? "bg-green-100 text-green-800"
                                                 : operator.status === "inactive"
-                                                  ? "bg-red-100 text-red-800"
-                                                  : "bg-yellow-100 text-yellow-800"
-                                        }`}
+                                                    ? "bg-red-100 text-red-800"
+                                                    : "bg-yellow-100 text-yellow-800"
+                                            }`}
                                     >
                                         {translateStatus(operator.status)}
                                     </span>
